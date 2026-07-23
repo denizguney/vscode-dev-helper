76349e86518768badbb2c514c3b0bf1ee72f4d92
Deniz Güney Yıldırım <31777283+denizguney@users.noreply.github.com>
2026-07-24 00:20:50 +0300

    Implement commands for VS Code extension
    Added commands to show a message and count selected characters in the active editor.

--- src/extension.ts
+++ src/extension.ts
@@ -1,1 +1,28 @@
 
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext){
+
+  console.log('Congratulations, your extension "vscode-dev-helper" is now active');
+  let disposable = vscode.commands.registerCommand('vscode-dev-helper.helloworld', () => {
+    vscode.window.showInformationMessage('Selam VS Code Dev Helper aktif ve çalışıyor');
+});
+
+let countChars = vscode.commands.registerCommmand('vscode-dev-helper.countChars' , () => {
+
+
+  const editor = vscode.window.activeTextEditor;
+
+  if(!editor){
+
+    vscode.window.showWarningMessage('Aktif bir editör bulunamadı');
+  }
+
+  const selection = editor.selection;
+  const text = editor.document.getText(selection);
+  vscode.windo.showInformationMessage('Seçilen Metin ${text.length} karakter uzunluğunda');
+});
+  context.subscriptions.push(disopsable);
+  context.subscriptions.push(countChars);
+}
+export function deactive() {}
